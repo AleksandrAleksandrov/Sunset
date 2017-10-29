92bdcf20e41f6120bf05db1f1bd4d6e3fa6b9d0c
AleksandrAleksandrov <aleksandrggggg@gmail.com>
2017-10-29 12:42:23 +0200

    Initial commit

---
+++ app/src/main/java/com/aleksandr/aleksandrov/project/test/android/sunset/SingleFragmentActivity.java
@@ -0,0 +1,36 @@
+package com.aleksandr.aleksandrov.project.test.android.sunset;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by aleksandr on 10/29/17.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_sunset;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
---
+++ app/src/main/java/com/aleksandr/aleksandrov/project/test/android/sunset/SunsetFragment.java
@@ -0,0 +1,27 @@
+package com.aleksandr.aleksandrov.project.test.android.sunset;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by aleksandr on 10/29/17.
+ */
+
+public class SunsetFragment extends Fragment {
+
+    public static SunsetFragment newInstance() {
+        return new SunsetFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sunset, container, false);
+
+        return view;
+    }
+}
